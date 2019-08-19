a39026e29388e584942d5dcc10f76b091bcadc9e
DinnerKang <jenhyuk0318@gmail.com>
2019-08-19 19:01:44 +0900

    event-bus-2 내장 함수 이용

---
+++ EventBus/event-bus-2/src/components/ByWorld.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button v-on:click="helloWorld">자식 버튼</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        helloWorld(){
+            this.$emit('hello-world', '안녀엉');
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ EventBus/event-bus-2/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ByWorld @hello-world="childMsg" ></ByWorld>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+import ByWorld from './ByWorld';
+
+export default {
+  name: 'HelloWorld',
+  components:{
+    ByWorld
+  },
+  data(){
+    return{
+      msg: '',
+    }
+  },
+  methods:{
+    childMsg(payload){
+      this.msg = payload;
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
