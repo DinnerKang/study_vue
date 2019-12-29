d2eedcfda9781d43082f70d8094455c2fdacc5ba
DinnerKang <jenhyuk0318@gmail.com>
2019-12-29 23:57:00 +0900

    my page 추가

---
+++ vue-pwa/src/views/MyPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="myPage_container">
+    마이페이지
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MyPage',
+}
+</script>
+<style scoped>
+  .home{
+    text-align: center;
+  }
+</style>
