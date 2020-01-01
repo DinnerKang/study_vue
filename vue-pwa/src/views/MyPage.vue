373a529e5493f0d6f19cb41b3bb95075f23ef8b5
DinnerKang <jenhyuk0318@gmail.com>
2020-01-01 23:12:57 +0900

    데이터 읽기 쓰기 구현 중

--- vue-pwa/src/views/MyPage.vue
+++ vue-pwa/src/views/MyPage.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="myPage_container">
-    <input type="text" placeholder="음악 이름" />
-    <input type="text" placeholder="유튜브 URL" />
-    <button type="submit" @click="registData">등록</button>
+    <h2>
+      My Page
+    </h2>
+    <h5>
+      {{ userName }}님 안녕하세요.
+    </h5>
+    <session class="regist_container">
+      <article>
+        <div>
+          <input type="text" v-model="musicName" placeholder="음악 이름" />
+          <input type="text" v-model="singerName" placeholder="가수 이름" />
+          <input type="text" v-model="youtubeURL" placeholder="유튜브 URL" />
+          <button type="submit" @click="registData">등록</button>
+        </div>
+      </article>
+    </session>
+    <session class="myMusic_container">
+      <article class="myMusic_list">
+        <ul>
+          <li v-for="(list, idx) in musicList" :key="idx">
+            {{list}}
+          </li>
+        </ul>
+      </article>
+    </session>
   </div>
 </template>
 
@@ -13,17 +35,33 @@ export default {
   name: 'MyPage',
   data(){
     return {
-       musicName: '',
-       youtubeURL: '',
+      userName: this.$store.state.userName,
+      musicName: '',
+      singerName: '',
+      youtubeURL: '',
+      
+      musicList: [],
     }
+  },
+  created(){
+    this.getMusicList();
   },
   methods: {
     registData() {
-        firebase.database().ref('myMusic/' + this.$store.state.username).set({
+        firebase.database().ref(`myMusic/${this.userName}`).push({
           musicName: this.musicName,
-          youtubeURL: this.youtubeURL || '',
+          singerName: this.singerName,
+          youtubeURL: this.youtubeURL,
+          time: new Date(),
         });
     },
+    getMusicList() {
+      firebase.database()
+          .ref(`myMusic/${this.userName}`)
+          .on('value', (snapshot) => {
+            this.musicList = snapshot.val();
+      });
+    }
   }
 }
 </script>
