a47c9956c305ee5be602d411bebc449d7a1b57d5
DinnerKang <jenhyuk0318@gmail.com>
2020-01-23 12:27:21 +0900

    melon list

---
+++ deali-music/src/components/List/MelonList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <ul>
+            <li class="melon_list" v-for="(list, idx) in melonList" :key="idx">
+                {{idx}}위<br />
+                음악 : {{list.title}}<br />
+                가수 : {{list.artist}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+export default {
+    data() {
+        return {
+            melonList: [],
+        };
+    },
+    created() {
+        this.getMelonChart();
+    },
+    methods: {
+        getMelonChart() {
+            firebase.database()
+                .ref(`melon`)
+                .on('value', (snapshot) => {
+                    const res = snapshot.val();
+                    console.log(res);
+                    this.melonList = res.result;
+            });
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .melon_list{
+        margin-top: 24px;
+    }
+</style>
--- deali-music/src/views/Home.vue
+++ deali-music/src/views/Home.vue
@@ -3,13 +3,18 @@
     <img class="logo_img" src="../assets/logo.png" alt="logo" >
     <router-link to="/myPage">마이페이지</router-link>
     <router-link to="/musicPlayer">음악듣기</router-link>
+    <melon-list></melon-list>
   </div>
 </template>
 
 <script>
+import MelonList from '../components/List/MelonList';
 
 export default {
   name: 'home',
+  components: {
+    MelonList
+  },
   created(){
     this.$axios.defaults.headers['authorization'] = `Bearer ${this.$store.state.accessToken}`;
   },
