1cb9551d86605b2eaf8db926aa5be8840f6cb9b4
DinnerKang <jenhyuk0318@gmail.com>
2020-03-19 18:06:49 +0900

    0319

--- deali-music/src/service/Storage.js
+++ deali-music/src/service/Storage.js
@@ -2,23 +2,11 @@
 import * as firebase from 'firebase/app';
 import 'firebase/storage';
 
+
 export function readFolderLists() {
     return firebase.storage().ref('thumbnail').listAll();
 }
 
 export function getThumbnail(fullPath) {
-    const url = firebase.storage().ref(`${fullPath}`).getDownloadURL();
-    return new Promise((resolve) => {
-        let xhr = new XMLHttpRequest()
-        xhr.onload = () => {
-          let reader = new FileReader()
-          reader.onloadend = function () {
-            resolve(reader.result)
-          }
-          reader.readAsDataURL(xhr.response)
-        }
-        xhr.open('GET', url)
-        xhr.responseType = 'blob'
-        xhr.send()
-    });
+    return firebase.storage().ref(fullPath);
 }
