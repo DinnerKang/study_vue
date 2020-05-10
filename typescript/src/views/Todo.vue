6a7b28fcd8a644dd409373a4f2d453813a86779e
DinnerKang <jenhyuk0318@gmail.com>
2020-05-10 19:18:14 +0900

    typescript

---
+++ typescript/src/views/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="todo-container">
+        <h2>To Do List 만들기</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+    name: 'todo',
+});
+</script>
+
+<style lang="scss" scoped>
+    .todo-container{
+        width: 100%;
+        height: 100vh;
+    }
+</style>
