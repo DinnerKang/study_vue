ba5d55c3de5d58690c8f6cc60cd3695e84348efb
DinnerKang <jenhyuk0318@gmail.com>
2020-07-05 15:34:58 +0900

    PWA GPS 받기

--- vue-pwa/src/main.js
+++ vue-pwa/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueCompositionApi from '@vue/composition-api';
+
+Vue.use(VueCompositionApi);
 
 Vue.config.productionTip = false
 
--- vue-pwa/src/views/Home.vue
+++ vue-pwa/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map" style="width:100%;height:400px;"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+//function that gets the location and returns it
+function getLocation() {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geo Location not supported by browser");
+  }
+}
+//function that retrieves the position
+function showPosition(position) {
+  var location = {
+    longitude: position.coords.longitude,
+    latitude: position.coords.latitude
+  }
+  console.log(location);
+  /*
+  map = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(location.latitude, location.longitude),
+      zoom: 15
+  });*/
+  alert(`${location.longitude}, ${location.latitude}`);
+}
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    //request for location
+    getLocation();
   }
 }
 </script>
