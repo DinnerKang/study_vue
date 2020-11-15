6155fce93bf7a58e938bb6694255b7bb1f3e77db
DinnerKang <jenhyuk0318@gmail.com>
2020-11-16 01:43:48 +0900

    서버단 준비

---
+++ todo-list/back/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/test', (req, res) => {
+    console.log(req.body);
+    return res.json({a: 1});
+});
+
+http.createServer(app).listen(5000, () => {
+    console.log('서버 시작');
+});
