67a4366364efb1467ef9d8c55616865baf71bec0
DinnerKang <jenhyuk0318@gmail.com>
2021-01-19 16:02:01 +0900

    DB 연동 로직 완료

--- todo-list/back/index.js
+++ todo-list/back/index.js
@@ -3,30 +3,69 @@ const cors = require('cors');
 const http = require('http');
 const jwt = require('jsonwebtoken');
 
+const mySql = require('mysql');
+const pool = mySql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'testDB',
+});
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-let user = [];
 const privateKey = 'dinner';
 
 app.post('/signUp', (req, res) => {
     console.log('회원 추가: ', req.body);
-    user.push(req.body);
-    console.log('회원: ', user);
-    return res.json({ result: 'success' });
+    const data = {
+        email: req.body.email,
+        password: req.body.password,
+    };
+    pool.getConnection((err, conn) => {
+        if (!err) {
+            conn.query('INSERT INTO user (email, password) VALUES (?,?)', [data.email, data.password], (err, rows) => {
+                if (err) {
+                    if (err.code === 'ER_DUP_ENTRY') {
+                        return res.status(500).json({ msg: '중복 아이디' });
+                    } else {
+                        return res.status(500).json({ result: err });
+                    }
+                };
+                return res.json({ result: 'success' });
+            });
+        }
+        conn.release();
+    });
 });
 app.post('/emailLogin', (req, res) => {
-    if (user.filter(i => i.email === req.body.email && i.password === req.body.password).length === 0) {
-        console.log('아이디 없듬');
-        return res.json({ result: '땡' });
-    }
-    const token = jwt.sign({ 
-        email: req.body.email,
-        info: '토큰에 넣고싶은거',
-     }, privateKey, { expiresIn: '1d' });
-     console.log('성공 ID: ', req.body.email);
-    return res.json({ token });
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(500).json({ result: '아이디나 비밀번호를 입력해주세요.' });
+    pool.getConnection((err, conn) => {
+        if (!err) {
+            conn.query('SELECT * FROM user WHERE email = ?', [email], (err, rows) => {
+                if (err) return res.status(500).json({ result: err });
+                if (rows.length === 0) return res.status(500).json({ result: '아이디가 없습니다.' });
+                if (rows[0].password === password) {
+                    const token = jwt.sign({ 
+                        email: req.body.email,
+                        info: '토큰에 넣고싶은거',
+                     }, privateKey, { expiresIn: '1d' });
+
+                     conn.query('UPDATE user SET access_token = ?, refresh_token = ? WHERE email = ?', [token, token, email], (err, rows) => {
+                        if (err) return res.status(500).json({ result: err });
+                     });
+
+                    return res.json({ msg: '로그인 성공', result: token });
+                } else {
+                    return res.status(500).json({ result: '비밀번호가 틀렸습니다.' });
+                }
+            });
+        }
+        conn.release();
+    });
 });
 
 http.createServer(app).listen(5000, () => {
